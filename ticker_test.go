a328280c36c7eeec5d3111c5794f6b4c604c73c8
Nathan Osman <nathan@quickmediasolutions.com>
2025-08-03 12:59:22 -0700

    Improve test for Ticker.Stop().

--- ticker_test.go
+++ ticker_test.go
@@ -15,23 +15,18 @@ func TestTicker(t *testing.T) {
 	Mock()
 	defer Unmock()
 	ticker := NewTicker(2 * time.Second)
+	defer ticker.Stop()
 	Advance(3 * time.Second)
 	<-ticker.C
 	Advance(2 * time.Second)
 	<-ticker.C
 }
 
-func TestTickerStop(t *testing.T) {
-	Mock()
-	defer Unmock()
-	ticker := NewTicker(1 * time.Second)
-	ticker.Stop()
-}
-
 func TestTickerReset(t *testing.T) {
 	Mock()
 	defer Unmock()
 	ticker := NewTicker(4 * time.Second)
+	defer ticker.Stop()
 	ticker.Reset(2 * time.Second)
 	Advance(3 * time.Second)
 	<-ticker.C
